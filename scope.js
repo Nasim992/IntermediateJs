3eacaf4983034943cdd210ac2f13e296d96af2d2
Nasim992 <mdnasim6416@gmail.com>
2020-08-11 10:42:07 +0000

    Scope

---
+++ scope.js
@@ -0,0 +1,5 @@
+// We cannot get output outside the function  
+
+Hoisting is to use the variable to the upper level. By using var we can get access the value  anywhere in the program.
+But if we use let or const then we can't get access because it is block scope. Using block scope we can access the value only inside the block.
+By using hoisting we can get all the access form any where in the program.  
