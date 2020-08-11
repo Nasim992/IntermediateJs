b4f72108d3b8d76f4ab950ecac1d3999a8dae110
Nasim992 <mdnasim6416@gmail.com>
2020-08-11 09:28:13 +0000

    doubleVsSingleEqual done

---
+++ doubleVsSingleEqual.js
@@ -0,0 +1,12 @@
+const first = 2 ;
+const second = "2" ;
+if( first === second ) {
+    console.log("condition is true ");
+}
+else {
+    console.log("condition is false")
+}
+
+// == means check only it's value 
+// === means check it's value and it's type 
+// In javaScript it's compare to it's 1 and true .that means 0 means false and 1 means true.
